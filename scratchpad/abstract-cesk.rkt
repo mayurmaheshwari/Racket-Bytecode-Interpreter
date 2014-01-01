d6d7ee642711531ded86f1cefdef8938ca24863a
Mayur Maheshwari <mayurm@Mayurs-MacBook-Pro.local>
2013-12-31 18:42:38 -0700

    0CFA Abstract CESK
    Store versions are used in program states. writes states graph.dot

--- scratchpad/abstract-cesk.rkt
+++ scratchpad/abstract-cesk.rkt
@@ -1,46 +1,69 @@
 #lang racket
 
+;; 0CFA inspired by David Van Horn and Matthew Might's paper:
+;; "Systematic abstraction of abstract machines."
+;; Journal of Functional Programming. 2012.
+
+;; CESK style interpretation inspired by Matthew Might's 
+;; concrete interpreter : http://matt.might.net/articles/cesk-machines/
+
+;; Abstract interpretation inspired by Matthew Might's 
+;; abstract interpreter : http://matt.might.net/articles/intro-static-analysis/
+
+;; Author: Mayur Maheshwari 
+;;        (mayurm@cs.utah.edu)
+
 
 ; Auxiliary functions:
 
-; update : (a -> b) a b -> (a -> b)
-(define (update f x y)
-  (λ (x*)
-    (if (equal? x x*)
-        y
-        (f x*))))
-
+; Global mutable hash-map for store
 (define store-map (make-hash))
 
-; weak updates to store hash-map
-; values are stored as list
-; values for same key are appended
-; Duplicate updates for same key are filtered
-(define (update-store x y)
-   (if(hash-has-key? store-map x)
-      (hash-set! store-map x 
-                 (if (member y (hash-ref store-map x))
-                     (hash-ref store-map x)
-                     (if (list? y)
-                         (append y (hash-ref store-map x))
-                         (append (list y) (hash-ref store-map x)))))
-      (hash-set! store-map x (list y)))
-   (λ (x*)
-     (hash-ref store-map x*)))
- 
+; Initial version to the store
+(hash-set! store-map 'version 0)
 
-; update* : (a -> b) [a] [b] -> (a -> b)
-(define (update* f xs ys)
-  (match* (xs ys)
-    [['() '()]    f]
-    [[(cons x xs*) (cons y ys*)]
-     (update* (update-store x y) xs* ys*)]))
+; Finds a #t value in a list of booleans
+; got-true : [value ...] -> boolean 
+(define (got-true? lst)
+    (and (not (null? lst))
+         (or (eq? #t (car lst))
+             (got-true? (cdr lst)))))
+
+
+; Weak updates to store
+; Updates are joined
+; Duplicate update values are eliminated
+; update-store : addr value -> Updated global store-map
+(define (update-store x y)
+  (cond
+    [(hash-has-key? store-map x)
+     (define old (hash-ref store-map x))
+     (cond
+       [(list? y)
+        (if (or (eqv? (car y) 'Ar)
+                (eqv? (car y) 'letk)
+                (eqv? (car y) 'Fn)
+                (eqv? (car y) 'if))
+            (cond
+            [(not (member y old))
+             (set! old (cons y old))])
+            (for ([new y])
+              (cond
+                [(not (member new old))
+                 (set! old (cons new old))])))
+        (hash-set! store-map x old)]
+       [else
+        (cond
+          [(not (member y old))
+           (set! old (cons y old))
+           (hash-set! store-map x old)])])]
+    [else (hash-set! store-map x (list y))]))
 
 
 
 ; A-Normal Form:
 
-; <lam> ::= (λ (<var> ...) <exp>)
+; <lam> ::= (λ (<var>) <exp>)
 
 ; <aexp> ::= <integer>
 ;         |  <var>
@@ -67,6 +90,7 @@
                         (loop))))]
        (loop))]))
 
+
 ; atom? : exp -> boolean?
 (define (atom? exp)
   (match exp
@@ -86,51 +110,74 @@
 
 ; CESK machine state:
 (struct astate {control        ; exp
-                store          ; store
+                store-version  ; store version
                 continuation}  ; kont
   #:transparent) ; enable low-level access to struct fields
 
 
-; σ : store = addr -> list of values
+; σ : store version
 
-; value = integer + boolean + clo + kont
+; value = list of (integer + boolean + clo)
 
 ; clo ::= (clo <lam>)
 
-; κ : kont ::= (letk <var> <exp> <kont>)
+; κ : kont ::= (letk <var> <exp> <kont-addr>)
 ;           |  (Ar <exp> <kont-addr>)
-;           |  (Fn <clo> <kont-addr>)
+;           |  (Fn <exp> <kont-addr>)
+;           |  (if <exp0> <exp1> <kont-addr> )
 ;           |  halt
 
-; addr = a set of addresses;
-;        variable symbols 'symbol to store values
-;        applications (e0 e1) to store Kont
+; addr = Variable names to store list 
+;        of values flowing into them ;
+;        Application (e0 e1) to store 
+;        kontinuations at call sites ;
+;        Unique symbols using gensym to 
+;        store continuations created w/ if and let stmts.
 
 
 
-; apply-kont : kont value store -> (state + answer)
+; apply-kont : kont [value ...] store-version 
+; -> 
+;   (display answer)
+; | [astates ...]
 (define (apply-kont κ value σ)
   (match κ
     ; Apply the halt continuation:
     ['halt
      (display `(answer ,value ,σ))]
     
-    ; Note: value is a list of closures.
-    ; Output: List of states parameterized by 
-    ; different closure values flowing into the functions.
+    
+    ; Apply the if continuation and branch
+    [`(if ,e0 ,e1 ,a)
+     (if(got-true? value)
+        (for*/list ([κ (hash-ref store-map a)])
+          (astate e0 σ κ))
+        (for*/list ([κ (hash-ref store-map a)])
+          (astate e1 σ κ)))]
+
+    
+    ; Apply the argument continuation
     [`(Ar ,e ,a)
      (for*/list ([v value])
        (astate e σ `(Fn ,v ,a)))]
     
-    ; Note: value is a list of args that could 
-    ; flow into the param of function.
+    
+    ; Apply the procedure continuation
     [`(Fn ,f ,a)
      (apply-proc f value σ a)]
     
+    
     ; Resume execution:
-    [`(letk ,v ,e ,κ)
+    [`(letk ,v ,e ,a)
      
-     (list (astate e (update-store v value) κ))]))
+     (update-store v value)
+     
+     ;update store version
+     (define new-ver (+ (hash-ref store-map 'version) 1))
+     (hash-set! store-map 'version new-ver)
+     
+     (for*/list ([κ (hash-ref store-map a)])
+       (astate e new-ver κ))]))
 
 
 ; prim->proc : symbol? -> procedure?
@@ -147,16 +194,21 @@
 
 
                    
-; eval-atom : aexp env store -> value
-(define (eval-atom atom σ)
+; eval-atom : aexp 
+; -> 
+; value | [value ...]
+(define (eval-atom atom)
   (match atom
-    [(? symbol?)    (σ atom)] ;outputs a list of values.
+    [(? symbol?)    (hash-ref store-map atom)]
     [(? boolean?)   atom]
     [(? integer?)   atom]
     
+    ; Apply primary operations on [value ...] 
+    ; Use permutation on the lists
+    ; Assumption: Input of 2 variables (2 lists of values)
     [(cons (and prim (? prim?)) rest)
      ; =>
-     (let ([args (map (eval-atom/curry σ) rest)])
+     (let ([args (map (eval-atom/curry) rest)])
        (define new-args (for/list ([a args])
                           (if (list? a)
                               a
@@ -172,34 +224,37 @@
     
     [else           (error "unknown atom")]))
 
+; eval-atom/curry :-> aexp -> value
+(define (eval-atom/curry)
+  (λ (atom)
+    (eval-atom atom)))
 
-; eval-atom/curry : env store -> aexp -> value
-(define (eval-atom/curry σ)
-  (λ (atom) 
-    (eval-atom atom σ)))
 
-
-; store0 is the initial (empty) store
-(define store0 (λ (addr) (error (format "unbound address: ~a" addr))))
 
 ; ainject : exp -> astate
 (define (ainject exp)
-  (astate exp store0 'halt))
+  (astate exp 0 'halt))
 
-; apply-proc : value [value] store kont -> (astate + answer)
+; apply-proc : value [value ...] store-version kont-addr -> [astate ...]
 (define (apply-proc proc args σ a)
     
     (match proc
       ; apply a closure:
       [`(clo (λ ,vars ,body))
-       ; =>
-       ; weak-update the store: currently supporting only 1 formal param.
-       ; TBD Remove carring for multi-argument support.
-       (define σ* (update-store (car vars) args))
+       ; =>    
+       ; Assumption : Single formal parameter
+       (update-store (car vars) args)
      
-       (for*/list ([κ (σ* a)])
-         (astate body σ* κ))]))
-  
+       ;update store version
+       
+       ;update store version
+       (define new-ver (+ (hash-ref store-map 'version) 1))
+       (hash-set! store-map 'version new-ver)
+       
+       (for*/list ([κ (hash-ref store-map a)])
+         (astate body new-ver κ))]))
+
+
 ; step : astate -> (astate + answer)
 (define (astep ς)
   (match ς
@@ -208,54 +263,75 @@
     [(astate (and atom (? atom?)) σ κ)
      ; =>
      ; evaluate the return value:
-     ; Note: It is a list of value/s
-     (define return-value (eval-atom atom σ))
+     (define return-value (eval-atom atom))
        
      (apply-kont κ return-value σ)]
         
     ; conditional evaluation:
     [(astate `(if ,cond ,cons ,alt) σ κ)
      ; =>
-     ;(if (eval-atom cond σ)
-         (list (astate cons σ κ)
-         (astate alt σ κ))]
+     (define kont-addr (gensym 'kont))
+     (update-store kont-addr κ)
+     
+     ;update store version
+     (define new-ver (+ (hash-ref store-map 'version) 1))
+     (hash-set! store-map 'version new-ver)
+     
+     (list (astate cond new-ver `(if ,cons ,alt ,kont-addr)))]
 
     
     [(astate `(letrec ([,vars ,aexps] ...) ,body) σ κ)
-     ; =>
-     ; allocate fresh addresses:
-     (define addrs (map gensym vars))
+     ; =>     
+     ; evaluate the expressions
+     (define values (map (eval-atom/curry) aexps))
      
-     ; evaluate the expressions with the *new* env:
-     (define values (map (eval-atom/curry σ) aexps))
+     ; update the store with list of variables and values
+     (for ([vr vars]
+           [vl values])
+       (update-store vr vl))
      
-     ; update the store:
-     (define σ* (update* σ vars values))
+     ;update store version
+     (define new-ver (+ (hash-ref store-map 'version) 1))
+     (hash-set! store-map 'version new-ver)
      
-     (list (astate body σ* κ))]
+     (list (astate body new-ver κ))]
     
     
     ; let-binding:
     [(astate `(let ([,v ,exp]) ,body) σ κ)
      ; =>
-     (list (astate exp σ `(letk ,v ,body ,κ)))]
+     ;Store continuation into store.
+     (define kont-addr (gensym 'letk))
+     (update-store kont-addr κ)
+     
+     ;update store version
+     (define new-ver (+ (hash-ref store-map 'version) 1))
+     (hash-set! store-map 'version new-ver)
+     
+     (list (astate exp new-ver `(letk ,v ,body ,kont-addr)))]
+    
     
     ; function application:
     [(astate `(,f . ,es) σ κ)
-     ; =>
-     
+     ; => 
      (define addr (append (list f) es))
-     (define σ* (update-store addr κ))
+     (update-store addr κ)
+     
+     
+     ;update store version
+     (define new-ver (+ (hash-ref store-map 'version) 1))
+     (hash-set! store-map 'version new-ver)
+     
      ; considering only 1 argument.
      (define e (car es))
-     (list (astate f σ* `(Ar ,e ,addr)))]))
+     (list (astate f new-ver `(Ar ,e ,addr)))]))
     
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; run : exp -> answer
+; analyze : exp -> answer
 (define (analyze prog)
   
   ; the initial abstract astate:
@@ -295,6 +371,10 @@
   (while (not (null? todo))
          (display (set-count visited))
          (display store-map)
+         (display "
+
+****************************************
+")
          
     (define curr (pop-todo))
     (when (not (seen? curr))
@@ -308,7 +388,6 @@ CURR : ")
 SUCCS : ")
       (display succs)
       (display "
-****************************************
 
 ")
       (when (list? succs)
@@ -318,8 +397,12 @@ SUCCS : ")
   ; return all visited states:
   (make-graph-file neighbors visited))
 
+
+; FILE I/O
+; make-graph-file : neighbors visited -> graph.dot
+; Prints the graph Depth-First
 (define (make-graph-file neighbors visited)
-  ; FILE I/O
+  
   (define out (open-output-file "graph.dot" #:exists 'update))
   (display "digraph control_flow {
 	node [shape = circle];" out)
@@ -333,7 +416,7 @@ SUCCS : ")
 
 ;(display label-hash)
 (display "
-" out)
+")
 (for ([key (hash-keys label-hash)])
   (write (hash-ref label-hash key) out)
   (display " [tooltip = \"" out)
@@ -353,6 +436,8 @@ SUCCS : ")
 
 (display "}" out)
 (close-output-port out))
+
+
 
 ; example
 (define fact-prog
